c590837177776cb77fe3d1d7042faa2d84c36304
Stefan Zier <szier@icloud.com>
2016-02-13 14:11:24 -0800

    Create, Edit, Delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,35 @@
 class PostsController < ApplicationController
   def index
-  
+    @posts = Post.all 
+  end
+
+  def new
+   @post = Post.new 
   end
 
   def create
     @post = Post.create(post_params)
+    redirect_to posts_path
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
 
