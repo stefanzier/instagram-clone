801b09c456ea410fb8fd2e4be3451e737b5a4f78
Stefan Zier <szier@icloud.com>
2016-02-13 12:10:16 -0800

    Initial Commit
    Includes Haml gem, paperclip gem, and Posts Controller/model

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,15 @@
+class PostsController < ApplicationController
+  def index
+  
+  end
+
+  def create
+    @post = Post.create(post_params)
+  end
+
+
+  private
+    def post_params
+      params.require(:post).permit(:caption, :image)
+    end
+end
